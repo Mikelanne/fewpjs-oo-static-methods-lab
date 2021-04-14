d7289e6936102dce2660810bf9609af3fccad081
Mikelanne <mikelanne.wood@gmail.com>
2021-04-14 18:22:06 -0400

    completed assignment

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ class Formatter {
   }
 
   static sanitize(string) {
-    // return (this.sanitize(string))
+    return string.replace(/[^a-z0-9-'' +]+/gi, '')
   }
 
   static titleize(string) {
-    console.log(string)
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = string.split(" ");
+    let array = [];
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        array.push(words[i][0].toUpperCase() + words[i].substr(1));
+      } else if (exceptions.includes(words[i])) {
+        array.push(words[i]);
+      } else {
+        array.push(words[i][0].toUpperCase() + words[i].substr(1));
+      }
+    }
+    return array.join(" ")
   }
 }
